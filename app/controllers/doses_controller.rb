11031057f94add9ac90a9cf67193cbea82a2dc4d
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-05-15 12:09:54 +0100

    got rid of buttons in favour of links

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,13 +1,11 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create]
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    # @ingredient = Ingredient.new(Ingredient.find())
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
     @dose.save
     redirect_to cocktail_path(@cocktail)
@@ -19,6 +17,10 @@ class DosesController < ApplicationController
   end
 
   private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
